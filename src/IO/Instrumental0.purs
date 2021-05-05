4f21977cd18e92383021e316b7a6a13232fcdcb5
Mike Solomon <mike@meeshkan.com>
2021-05-05 18:25:06 +0300

    Changes instrumental to record

--- src/IO/Instrumental0.purs
+++ src/IO/Instrumental0.purs
@@ -3,25 +3,26 @@ module SambaDeUmaNotaSo.IO.Instrumental0 where
 import Data.List (List)
 import SambaDeUmaNotaSo.Types (VideoSpan)
 
-data Instrumental0
-  = Circle0
-  | Circle1
-  | Circle2
-  | Circle3
-  | Circle4
-  | Circle5
-  | Circle6
-  | Circle7
-  | Circle8
-  | Circle9
-  | Circle10
-  | Circle11
-  | Center
-  | Ring0
-  | Ring1
-  | Outside
+type Instrumental0 a
+  = { circle0 :: a
+    , circle1 :: a
+    , circle2 :: a
+    , circle3 :: a
+    , circle4 :: a
+    , circle5 :: a
+    , circle6 :: a
+    , circle7 :: a
+    , circle8 :: a
+    , circle9 :: a
+    , circle10 :: a
+    , circle11 :: a
+    , center :: a
+    , ring0 :: a
+    , ring1 :: a
+    , outside :: a
+    }
 
 type Accumulator
   = { videoSpan :: VideoSpan
-    , activeZones :: List { time :: Number, instrument :: Instrumental0 }
+    , activeZones :: Instrumental0 (List Number)
     }
