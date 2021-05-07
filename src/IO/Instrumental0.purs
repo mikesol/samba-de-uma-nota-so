bf6f293c30e29a147e9c745ef3139c44381a61d4
Mike Solomon <mike@meeshkan.com>
2021-05-07 11:03:50 +0300

    Starts on paintings for instrumental 0

--- src/IO/Instrumental0.purs
+++ src/IO/Instrumental0.purs
@@ -4,18 +4,18 @@ import Data.List (List)
 import SambaDeUmaNotaSo.Types (VideoSpan)
 
 type Instrumental0 a
-  = { circle0 :: a
-    , circle1 :: a
-    , circle2 :: a
-    , circle3 :: a
-    , circle4 :: a
-    , circle5 :: a
-    , circle6 :: a
-    , circle7 :: a
-    , circle8 :: a
-    , circle9 :: a
-    , circle10 :: a
-    , circle11 :: a
+  = { wedge0 :: a
+    , wedge1 :: a
+    , wedge2 :: a
+    , wedge3 :: a
+    , wedge4 :: a
+    , wedge5 :: a
+    , wedge6 :: a
+    , wedge7 :: a
+    , wedge8 :: a
+    , wedge9 :: a
+    , wedge10 :: a
+    , wedge11 :: a
     , center :: a
     , ring0 :: a
     , ring1 :: a
--- src/Transitions/Instrumental0Paintings.purs
+++ src/Transitions/Instrumental0Paintings.purs
@@ -8,14 +8,40 @@ import Data.List ((:), List(..))
 import Data.Maybe (Maybe(..))
 import Data.NonEmpty ((:|))
 import Data.Tuple.Nested ((/\))
-import Graphics.Painting (Painting, arc, fillColor, filled, translate, withMove)
+import Graphics.Painting (Painting, arc, circle, fillColor, filled, lineWidth, outlineColor, outlined, rectangle, translate, withMove)
 import Math (pi)
 import SambaDeUmaNotaSo.Constants (beats)
 import SambaDeUmaNotaSo.Util (NonEmptyToCofree, nonEmptyToCofree)
 import Web.HTML.HTMLElement (DOMRect)
 
 type IPContext a
-  = Reader { canvas :: DOMRect, halfW :: Number, halfH :: Number, mwh :: Number } a
+  = Reader
+      { canvas :: DOMRect
+      , halfW :: Number
+      , halfH :: Number
+      , mwh :: Number
+      }
+      a
+
+background :: IPContext Painting
+background = do
+  { canvas: { width, height } } <- ask
+  pure $ filled (fillColor (rgb 24 83 76)) (rectangle 0.0 0.0 width height)
+
+innerCircle :: IPContext Painting
+innerCircle = do
+  { halfW, halfH, mwh } <- ask
+  pure $ filled (fillColor (rgb 200 200 200)) (circle halfW halfH (mwh * 0.06))
+
+ring0 :: IPContext Painting
+ring0 = do
+  { halfW, halfH, mwh } <- ask
+  pure $ outlined (outlineColor (rgb 100 100 100) <> lineWidth (mwh * 0.05)) (circle halfW halfH (mwh * 0.25))
+
+ring1 :: IPContext Painting
+ring1 = do
+  { halfW, halfH, mwh } <- ask
+  pure $ outlined (outlineColor (rgb 100 100 100) <> lineWidth (mwh * 0.05)) (circle halfW halfH (mwh * 0.34))
 
 singleWedge :: Int -> IPContext Painting
 singleWedge n = do
@@ -25,12 +51,3 @@ singleWedge n = do
   st = toNumber n / 6.0
 
   ed = toNumber (n + 1) / 6.0
-
-wedge13 :: DOMRect -> Painting
-wedge13 { width, height } = (translate (width * -0.000) (height * 0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 1.1666666666666667) (pi * 1.3333333333333333) (1.000 * mwh * 0.45)))) <> (translate (width * 0.000) (height * 0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 0.0) (pi * 0.16666666666666666) (1.000 * mwh * 0.45)))) <> (translate (width * 0.000) (height * -0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 1.5) (pi * 1.6666666666666667) (1.000 * mwh * 0.45)))) <> (translate (width * -0.000) (height * -0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 0.3333333333333333) (pi * 0.5) (1.000 * mwh * 0.45)))) <> (translate (width * 0.000) (height * -0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 0.8333333333333334) (pi * 1.0) (1.000 * mwh * 0.45)))) <> (translate (width * -0.000) (height * 0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 1.3333333333333333) (pi * 1.5) (1.000 * mwh * 0.45)))) <> (translate (width * 0.000) (height * 0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 0.5) (pi * 0.6666666666666666) (1.000 * mwh * 0.45)))) <> (translate (width * 0.000) (height * -0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * -0.16666666666666666) (pi * 0.0) (1.000 * mwh * 0.45)))) <> (translate (width * -0.000) (height * -0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 0.16666666666666666) (pi * 0.3333333333333333) (1.000 * mwh * 0.45)))) <> (translate (width * -0.000) (height * 0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 1.0) (pi * 1.1666666666666667) (1.000 * mwh * 0.45)))) <> (translate (width * 0.000) (height * 0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 0.6666666666666666) (pi * 0.8333333333333334) (1.000 * mwh * 0.45)))) <> (translate (width * -0.000) (height * -0.000) $ filled (fillColor (rgb 255 255 255)) (withMove halfW halfH true (arc halfW halfH (pi * 1.6666666666666667) (pi * 1.8333333333333333) (1.000 * mwh * 0.45))))
-  where
-  halfW = width / 2.0
-
-  halfH = height / 2.0
-
-  mwh = min width height
