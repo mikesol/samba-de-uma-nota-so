09774cf68f8cd30b5d66a9a45d7c2ea92c105bd8
Mike Solomon <mike@meeshkan.com>
2021-05-20 14:32:09 +0300

    Uses headroom

--- src/ASDR.purs
+++ src/ASDR.purs
@@ -3,6 +3,7 @@ module SambaDeUmaNotaSo.ASDR where
 import Prelude
 import SambaDeUmaNotaSo.Instrumental0Paintings (halfBeat)
 import SambaDeUmaNotaSo.Util (calcSlope)
+import WAGS.Graph.Parameter (AudioParameter(..), defaultParam, param)
 
 quaverASR0Attack = 0.07 :: Number
 
@@ -10,8 +11,17 @@ quaverASR0Decay = 0.14 :: Number
 
 quaverASR0Low = 0.27 :: Number
 
-quaverASR0 :: Number -> Number
-quaverASR0 time
-  | time < quaverASR0Attack = calcSlope 0.0 0.0 quaverASR0Attack 1.0 time
-  | time < quaverASR0Decay = calcSlope quaverASR0Attack 1.0 quaverASR0Decay quaverASR0Low time
-  | otherwise = calcSlope quaverASR0Decay quaverASR0Low halfBeat 0.0 time
+quaverASR0 :: Number -> Number -> AudioParameter
+quaverASR0 time headroom
+  | time < quaverASR0Attack =
+    if time + headroom >= quaverASR0Attack then
+      AudioParameter
+        ( defaultParam
+            { param = 1.0
+            , timeOffset = quaverASR0Attack - time
+            }
+        )
+    else
+      param $ calcSlope 0.0 0.0 quaverASR0Attack 1.0 time
+  | time < quaverASR0Decay = param $ calcSlope quaverASR0Attack 1.0 quaverASR0Decay quaverASR0Low time
+  | otherwise = param $ calcSlope quaverASR0Decay quaverASR0Low halfBeat 0.0 time
